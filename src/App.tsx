d0c17d9aefe18b037bd774c773d40852c8470214
yunchan123 <phenomenal312@hufs.ac.kr>
2024-03-26 12:50:25 +0900

    feat: router

--- src/App.tsx
+++ src/App.tsx
@@ -1,5 +1,41 @@
+import { RouterProvider, createBrowserRouter } from "react-router-dom";
+import Layout from "./components/layout";
+import Home from "./routes/home";
+import Profile from "./routes/profile";
+import Login from "./routes/login";
+import Create from "./routes/create-account";
+
+const router = createBrowserRouter([
+  {
+    path: "/",
+    element: <Layout />,
+    children: [
+      {
+        path: "",
+        element: <Home />,
+      },
+      {
+        path: "profile",
+        element: <Profile />,
+      },
+    ],
+  },
+  {
+    path: "/login",
+    element: <Login />,
+  },
+  {
+    path: "/create-account",
+    element: <Create />,
+  },
+]);
+
 function App() {
-  return <div className="border-2 border-black">wowo</div>;
+  return (
+    <>
+      <RouterProvider router={router} />
+    </>
+  );
 }
 
 export default App;
