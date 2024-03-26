ee9814ee291ea5aa2e97c41b91b1b7cb391b826b
yunchan123 <phenomenal312@hufs.ac.kr>
2024-03-26 19:49:17 +0900

    feat: logout, protectedRout

--- src/routes/home.tsx
+++ src/routes/home.tsx
@@ -1,7 +1,13 @@
+import { auth } from "../firebase";
+
 export default function Home() {
+  const logOut = () => {
+    auth.signOut();
+  };
   return (
     <>
       <h1>Home!</h1>
+      <button onClick={logOut}>logout</button>
     </>
   );
 }
