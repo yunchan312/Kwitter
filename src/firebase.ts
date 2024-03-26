f7761528f72353fe4a4e7fa21578eb7530e6aaa2
yunchan123 <phenomenal312@hufs.ac.kr>
2024-03-26 13:15:35 +0900

    feat: initialize firebase

---
+++ src/firebase.ts
@@ -0,0 +1,17 @@
+// Import the functions you need from the SDKs you need
+import { initializeApp } from "firebase/app";
+// TODO: Add SDKs for Firebase products that you want to use
+// https://firebase.google.com/docs/web/setup#available-libraries
+
+// Your web app's Firebase configuration
+const firebaseConfig = {
+  apiKey: "AIzaSyAoMInCgoQa2-69t9ddyStwLjPejx7D7Zw",
+  authDomain: "kwitter-3b810.firebaseapp.com",
+  projectId: "kwitter-3b810",
+  storageBucket: "kwitter-3b810.appspot.com",
+  messagingSenderId: "304309992042",
+  appId: "1:304309992042:web:371845480fb1f934e3e813",
+};
+
+// Initialize Firebase
+const app = initializeApp(firebaseConfig);
